bbeab1c6e249c773448e8ba6535dd19abe5e3d41
Dustin Espinoza <dustin.espinoza.htz@gmail.com>
2019-12-08 20:03:03 -0600

    Setup and creation of ProductyType and Product models

---
+++ Data/ProductsAssignmentAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProductsAssignmentAPI.Models;
+
+namespace ProductsAssignmentAPI.Data
+{
+    public class ProductsAssignmentAPIContext : DbContext
+    {
+        public ProductsAssignmentAPIContext (DbContextOptions<ProductsAssignmentAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProductsAssignmentAPI.Models.ProductType> ProductType { get; set; }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductsAssignmentAPI.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+        public int ProductTypeId { get; set; }
+        public virtual ProductType ProductType { get; set; }
+        public string Notes { get; set; }
+        public decimal Price { get; set; }
+    }
+}
